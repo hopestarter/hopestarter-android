c463689fb454ab4d4bb1b0896b80870aea4537d9
Adrian Perez Heredia <aperezhrd@gmail.com>
2016-08-30 10:38:06 +0200

    Removed unused imports

--- wallet/src/org/hopestarter/wallet/ui/ProfileFragment.java
+++ wallet/src/org/hopestarter/wallet/ui/ProfileFragment.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
